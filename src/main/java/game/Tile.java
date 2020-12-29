08f3997218aaa1ff5d07a0a4e554f4431818d5c8
Saad Ismail <saad.ismail@careem.com>
2020-12-29 10:48:10 +0500

    setup kata with pom and gitignore

---
+++ src/main/java/game/Tile.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Tile
+{
+    public int X;
+    public int Y;
+    public char Symbol;
+}
---
+++ src/test/java/GameShould.java
@@ -0,0 +1,132 @@
+import game.Game;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class GameShould {
+    private Game game;
+
+    @Before
+    public void InitializeGame(){
+        game = new Game();
+    }
+
+    @Test
+    public void NotAllowPlayerOToPlayFirst() {
+        assertThrows(Exception.class, () -> game.Play('O', 0, 0));
+    }
+
+    @Test
+    public void NotAllowPlayerXToPlayTwiceInARow() {
+        assertThrows(Exception.class, () ->{
+            game.Play('X', 0, 0);
+            game.Play('X', 1, 0);
+        });
+    }
+
+    @Test
+    public void NotAllowPlayerToPlayInLastPlayedPosition() {
+        assertThrows(Exception.class, () ->{
+            game.Play('X', 0, 0);
+            game.Play('O', 0, 0);
+        });
+    }
+
+    @Test
+    public void NotAllowPlayerToPlayInAnyPlayedPosition() {
+        assertThrows(Exception.class, () ->{
+            game.Play('X', 0, 0);
+            game.Play('O', 1, 0);
+            game.Play('X', 0, 0);
+        });
+    }
+
+    @Test
+    public void DeclarePlayerXAsAWinnerIfThreeInTopRow() throws Exception
+    {
+        game.Play('X', 0, 0);
+        game.Play('O', 1, 0);
+        game.Play('X', 0, 1);
+        game.Play('O', 1, 1);
+        game.Play('X', 0, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('X', winner);
+    }
+
+    @Test
+    public void DeclarePlayerOAsAWinnerIfThreeInTopRow() throws Exception
+    {
+        game.Play('X', 2, 2);
+        game.Play('O', 0, 0);
+        game.Play('X', 1, 0);
+        game.Play('O', 0, 1);
+        game.Play('X', 1, 1);
+        game.Play('O', 0, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('O', winner);
+    }
+
+    @Test
+    public void DeclarePlayerXAsAWinnerIfThreeInMiddleRow() throws Exception
+    {
+        game.Play('X', 1, 0);
+        game.Play('O', 0, 0);
+        game.Play('X', 1, 1);
+        game.Play('O', 0, 1);
+        game.Play('X', 1, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('X', winner);
+    }
+
+    @Test
+    public void DeclarePlayerOAsAWinnerIfThreeInMiddleRow() throws Exception
+    {
+        game.Play('X', 0, 0);
+        game.Play('O', 1, 0);
+        game.Play('X', 2, 0);
+        game.Play('O', 1, 1);
+        game.Play('X', 2, 1);
+        game.Play('O', 1, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('O', winner);
+    }
+
+    @Test
+    public void DeclarePlayerXAsAWinnerIfThreeInBottomRow() throws Exception
+    {
+        game.Play('X', 2, 0);
+        game.Play('O', 0, 0);
+        game.Play('X', 2, 1);
+        game.Play('O', 0, 1);
+        game.Play('X', 2, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('X', winner);
+    }
+
+    @Test
+    public void DeclarePlayerOAsAWinnerIfThreeInBottomRow() throws Exception
+    {
+        game.Play('X', 0, 0);
+        game.Play('O', 2, 0);
+        game.Play('X', 1, 0);
+        game.Play('O', 2, 1);
+        game.Play('X', 1, 1);
+        game.Play('O', 2, 2);
+
+        char winner = game.Winner();
+
+        assertEquals('O', winner);
+    }
+}
