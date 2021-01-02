65dc796af77707e5988d8d6c9043a06b83376aa0
Fahad Ahmed Khan <fahad.khan@careem.com>
2021-01-02 23:21:14 +0500

    Removed optionals

--- src/main/java/game/Board.kt
+++ src/main/java/game/Board.kt
@@ -5,33 +5,30 @@ class Board {
     private val tiles = mutableListOf<Tile>()
 
     init {
-        for (i in 0..2) {
-            for (j in 0..2) {
-                val tile = Tile(i, j)
-                tiles.add(tile)
-            }
-        }
+        for (i in 0..2)
+            for (j in 0..2)
+                tiles.add(Tile(i, j))
     }
 
-    private fun tilesIn(row: Int): List<Tile> {
-        return tiles.filter { row == it.x }
-    }
-
-    fun tileAt(x: Int, y: Int): Tile? {
-        return tiles.find { it.isAt(x, y) }
+    fun tileAt(x: Int, y: Int): Tile {
+        return tiles.find { it.isAt(x, y) }!!
     }
 
     fun mark(symbol: Symbol, x: Int, y: Int) {
-        tileAt(x, y)?.mark(symbol)
+        tileAt(x, y).mark(symbol)
     }
 
     fun hasWinnerIn(row: Int): Boolean {
         return hasAllTilesMarkedIn(row) && hasSameSymbolIn(row)
     }
 
+    private fun tilesIn(row: Int): List<Tile> {
+        return tiles.filter { row == it.x }
+    }
+
     private fun hasSameSymbolIn(row: Int): Boolean {
-        return tileAt(row, 0)!!.hasSameSymbolAs(tileAt(row, 1))
-               && tileAt(row, 2)!!.hasSameSymbolAs(tileAt(row, 1))
+        return tileAt(row, 0).hasSameSymbolAs(tileAt(row, 1))
+               && tileAt(row, 2).hasSameSymbolAs(tileAt(row, 1))
     }
 
     private fun hasAllTilesMarkedIn(row: Int): Boolean {
@@ -39,10 +36,10 @@ class Board {
     }
 
     fun hasTileMarkedAt(row: Int, i: Int): Boolean {
-        return tileAt(row, i)?.isMarked ?: false
+        return tileAt(row, i).isMarked
     }
 
-    fun symbolAt(x: Int, y: Int): Symbol? {
-        return tileAt(x, y)?.symbol
+    fun symbolAt(x: Int, y: Int): Symbol {
+        return tileAt(x, y).symbol
     }
 }
