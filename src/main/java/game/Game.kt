5235fd1b954cc05021d2038201ddb136407b5b18
Fahad Ahmed Khan <fahad.khan@careem.com>
2021-01-02 22:50:56 +0500

    Converted Game to Kotlin

--- src/main/java/game/Game.kt
+++ src/main/java/game/Game.kt
@@ -1,47 +1,45 @@
-package game;
+package game
 
-public class Game {
+class Game {
 
-    private final Board board = new Board();
-    private Symbol lastSymbol = Symbol.BLANK;
+    private val board = Board()
+    private var lastSymbol = Symbol.BLANK
 
-    public void play(char symbol, int x, int y) throws Exception {
-        play(Symbol.from(symbol), x, y);
+    fun play(symbol: Char, x: Int, y: Int) {
+        play(Symbol.from(symbol), x, y)
     }
 
-    public void play(Symbol symbol, int x, int y) throws Exception {
-        guardAgainstInvalidFirstPlayer(symbol);
-        guardAgainstRepeatedTurns(symbol);
-        guardAgainstInvalidPosition(x, y);
+    fun play(symbol: Symbol, x: Int, y: Int) {
+        guardAgainstInvalidFirstPlayer(symbol)
+        guardAgainstRepeatedTurns(symbol)
+        guardAgainstInvalidPosition(x, y)
 
-        lastSymbol = symbol;
-        board.mark(symbol, x, y);
+        lastSymbol = symbol
+        board.mark(symbol, x, y)
     }
 
-    private void guardAgainstInvalidFirstPlayer(Symbol newSymbol) throws InvalidFirstPlayerException {
-        if (lastSymbol.isBlank() && newSymbol.isO()) {
-            throw new InvalidFirstPlayerException();
+    private fun guardAgainstInvalidFirstPlayer(newSymbol: Symbol) {
+        if (lastSymbol.isBlank && newSymbol.isO) {
+            throw InvalidFirstPlayerException()
         }
     }
 
-    private void guardAgainstRepeatedTurns(Symbol newSymbol) throws RepeatedPlayerException {
+    private fun guardAgainstRepeatedTurns(newSymbol: Symbol) {
         if (newSymbol == lastSymbol) {
-            throw new RepeatedPlayerException();
+            throw RepeatedPlayerException()
         }
     }
 
-    private void guardAgainstInvalidPosition(int x, int y) throws InvalidPositionException {
+    private fun guardAgainstInvalidPosition(x: Int, y: Int) {
         if (board.hasTileMarkedAt(x, y)) {
-            throw new InvalidPositionException();
+            throw InvalidPositionException()
         }
     }
 
-    public char winner() {
-        if (board.hasWinnerInRow(0)) return board.symbolAt(0, 0).character();
-        if (board.hasWinnerInRow(1)) return board.symbolAt(1, 0).character();
-        if (board.hasWinnerInRow(2)) return board.symbolAt(2, 0).character();
-        return Symbol.BLANK.character();
+    fun winner(): Char {
+        if (board.hasWinnerInRow(0)) return board.symbolAt(0, 0).character()
+        if (board.hasWinnerInRow(1)) return board.symbolAt(1, 0).character()
+        if (board.hasWinnerInRow(2)) return board.symbolAt(2, 0).character()
+        return Symbol.BLANK.character()
     }
-
 }
-
